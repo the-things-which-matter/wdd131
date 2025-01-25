4d7aa4af20ca69272c959def0956c3bfebaa47b2
the-things-which-matter <igabrigham9@gmail.com>
2025-01-25 15:11:56 +0300

    fixed nav home page menu

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,8 @@
-// Update the footer with the current year and last modified date
+
 document.getElementById("currentyear").textContent = new Date().getFullYear();
 document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
 
-// Hamburger menu toggle functionality
+
 const menuButton = document.getElementById("menuButton");
 const menu = document.getElementById("menu");
 
