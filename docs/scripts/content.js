f9c12aa87ba959de8e0a12d2fadb679f5bdbef24
the-things-which-matter <igabrigham9@gmail.com>
2025-02-17 13:51:11 +0300

    added css abd html

--- docs/scripts/content.js
+++ docs/scripts/content.js
@@ -1,3 +1,8 @@
+
+
 document.addEventListener("DOMContentLoaded", function() {
     console.log("JavaScript Loaded");
 });
+
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
